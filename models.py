dedba341a515d9d9a9b2c3eca3bb783b57085645
ppou_hack <ssdh1009@gmail.com>
2020-09-05 15:10:49 +0900

    feat: model.py relationship
    user, todo의  관계 설정
    https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database

--- models.py
+++ models.py
@@ -6,6 +6,8 @@ class Todo(db.Model):
     ___tablename__ = 'todo'
 
     id = db.Column(db.Integer, primary_key=True)
+    # fcuser와 연결
+    fcuser_id = db.Column(db.Integer, db.ForeignKey('fcuser.id'), ullable=False)
     title = db .Column(db.String(256))
     tstamp = db.Column(db.DateTime, server_default=db.func.now())
 
@@ -16,3 +18,12 @@ class Todo(db.Model):
             'title': self.title,
             'tstamp': self.tstamp
         }
+
+class Fcuser(db.Model):
+    __tablename__ = 'fcuser'
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String(32))
+    password = db.Column(db.String(128))
+
+    # 관계 지정
+    todos = db.relationship('Todo',backref='fcuser',lazy=True)
