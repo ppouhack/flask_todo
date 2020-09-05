fae5fc95460e0b56736502594fb67838103f6dba
ppou_hack <ssdh1009@gmail.com>
2020-09-05 14:37:27 +0900

    feat: init, models.py add
    init 코드, DB models 추가

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+import os
 from flask import Flask
+from flask import request
+from models import db
 
 app = Flask(__name__)
 
+# 데이터베이스 생성
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'hi'
 
+db.init_app(app)
+db.app = app
+db.create_all()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5000, debug=True)
